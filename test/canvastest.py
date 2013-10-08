e2ac47817aca30b7b426846651938f30fd18a693
Bill Waggoner <ctgreybeard@gmail.com>
2013-10-08 18:26:40 -0400

    Canvas test
    Successful canvas test
    Includes scrolling logic
    Need to turn this into a class
    
    Signed-off-by: Bill Waggoner <ctgreybeard@gmail.com>

--- test/canvastest.py
+++ test/canvastest.py
@@ -19,8 +19,42 @@ class contentrow(tk.Frame):
 		self.grid()
 		print(msg)
 
-root = tk.Tk()
-root.title = 'Canvas Test'
+def pit():
+	print('Height is {}, canvasy of that is {}, scroll bottom is {}'.format(canvas.winfo_height(), canvas.canvasy(canvas.winfo_height()), canvas.cget('scrollregion').split()[3]))
+
+def doscroll(scroll = None, amount = None, type = None):
+
+	action = 'Ignored'	
+	if canvas.winfo_height() < canvas.bbox(contentid)[3]:
+		action = 'Scrolled'
+		canvas.yview(scroll, amount, type)
+
+	print('Scroll type={}, amount={}, type={} - {}'.format(scroll, amount, type, action))
+	
+def mousescroll(event):
+	print('Event={}, time={}'.format(event.type, event.time))
+	if event.type == '4':	# Button press
+		print('Button {}, by {}'.format(event.num, event.delta))
+		if event.num == 4:	# Scroll down
+			doscroll(tk.SCROLL, -1, tk.UNITS)
+		else:
+			doscroll(tk.SCROLL, 1, tk.UNITS)
+	elif event.type == '38':	# mousewheel
+		print('MouseWheel, by {}'.format(event.delta))
+	else:	# Huh?
+		print('Wrong event type: {}'.format(event.type)) 
+
+def configure(event):
+	print('Configure, new height={}'.format(event.height))
+
+root2 = tk.Tk()
+root2.title('Another window that does nothing and has nothing in it')
+f2 = tk.Frame(root2, width = 150, height = 150)
+f2.grid_propagate(False)
+f2.grid()
+
+root = tk.Toplevel()
+root.title('Canvas Test')
 
 # Holds the "labels"
 labelframe = tk.Frame(root, border = 2)
@@ -31,7 +65,7 @@ label = tk.Label(labelframe, border = 2, text = 'ThisIsALabel ' * 4)
 masterframe = tk.Frame(root, border = 2, background = '#e0e')                             # Purple
 
 canvas = tk.Canvas(masterframe, 
-	yscrollincrement = 10,
+	yscrollincrement = 20,
 	width = 390,
 	background = '#ee0')                # BRIGHT YELLOW
 scrollbar = tk.Scrollbar(masterframe, orient = tk.VERTICAL)
@@ -76,7 +110,24 @@ buttonclose.grid(column = 1, row = 0)
 contentid = canvas.create_window(0, 0, anchor = tk.NW, window = contentframe)
 
 canvas.configure(yscrollcommand = scrollbar.set)
-scrollbar.configure(command = canvas.yview)
+scrollbar.configure(command = doscroll)
+
+canvas.update_idletasks()
+canvas.configure(scrollregion = (0, 0, 0, 0))
+print('Initial bbox=', canvas.bbox(contentid))
+masterframe.bind('<Configure>', configure)
+
+def bindscroll(c):
+	c.bind('<Button-4>', mousescroll)
+	c.bind('<Button-5>', mousescroll)
+	c.bind('<MouseWheel>', mousescroll)
+
+def deep_bind(c, bindf):
+	bindf(c)
+	for d in c.winfo_children():
+		bindf(d)
+
+deep_bind(root, bindscroll)
 
 num = 0
 
@@ -84,9 +135,18 @@ def doGo():
 	global num
 	new1 = contentrow(contentframe, num)
 	new1.update_idletasks()
+	
+	deep_bind(new1, bindscroll)
+
+	isScroll = int(canvas.canvasy(canvas.winfo_height())) >= int(canvas.cget('scrollregion').split()[3]) - 5
+	
 	canvas.configure(scrollregion = (0, 0, 0, 
-		canvas.bbox(contentid)[3] - canvas.winfo_height() % int(canvas.cget('yscrollincrement'))))
-	canvas.yview_scroll(int(new1.winfo_height() / int(canvas.cget('yscrollincrement'))), tk.UNITS)
+		canvas.bbox(contentid)[3])) # - canvas.winfo_height() % int(canvas.cget('yscrollincrement'))))
+	
+	if isScroll:
+		print('Scrolling...')
+		doscroll(tk.SCROLL, int(new1.winfo_height() / int(canvas.cget('yscrollincrement'))) + 1, tk.UNITS)
+		
 	print('contentframe is now {}, scrollregion is {}'.format(
 		canvas.bbox(contentid), 
 		canvas.cget('scrollregion')))
@@ -99,5 +159,6 @@ def doClose():
 buttongo.configure(command = doGo)
 buttonclose.configure(command = doClose)
 
-root.update()
-#root.mainloop()
+#root.update()
+root.mainloop()
+
